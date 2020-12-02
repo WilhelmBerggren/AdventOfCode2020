888558d302d885d064c186e14382c3a01e4d3d3f
WilhelmBerggren <wilhelmberggren@hotmail.com>
2020-12-03 00:33:35 +0100

    changed fizzbuzz for some reason

--- fizzbuzz.clj
+++ fizzbuzz.clj
@@ -1,9 +1,8 @@
-(let [f #(zero? (mod %2 %1))
-      fb #(condp f %
-            15 "fizzbuzz"
-            3 "fizz"
-            5 "buzz"
-            %)]
-  (map 
-   #(fb %) 
-   (range 1 101)))
+(map
+ (fn [n] 
+   (condp #(zero? (mod %2 %1)) n
+     15 "fizzbuzz"
+     3 "fizz"
+     5 "buzz"
+     n))
+ (range 1 101))
