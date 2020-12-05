9bdc1a8693e438194ccd165f5098408056a4d982
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-12-05 19:55:12 +0100

    day 5 c#

---
+++ advent5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var data = File.ReadAllText(Environment.CurrentDirectory + "/../input5.txt").Split("\n");
+
+var ids = data.Select(item => {
+    var row = Convert.ToInt32(item.Substring(0, 7).Replace('F', '0').Replace('B', '1'), 2);
+    var col = Convert.ToInt32(item.Substring(7, 3).Replace('L', '0').Replace('R', '1'), 2);
+    return row * 8 + col;
+}).ToArray();
+
+System.Console.WriteLine(ids.Max());
+
+Array.Sort(ids);
+
+var min = ids.First();
+for (int i = 1; i < ids.Length; i++)
+{
+    if (ids[i] != (ids[i-1] + 1)) {
+        System.Console.WriteLine(ids[i] - 1);
+    }
+}
