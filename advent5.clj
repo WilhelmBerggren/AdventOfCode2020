a61539650d70564037c80c794a09e99b5d8db673
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-12-05 23:15:52 +0100

    clojure day 5

---
+++ advent5.clj
@@ -0,0 +1,19 @@
+(def data (str/split (slurp "input5.txt") #"\n"))
+
+(defn to-int [item]
+  (Integer/parseInt 
+   (str/join 
+    (map #(case % \B 1 \R 1 0) item)) 
+   2))
+
+(def ids (sort (map to-int data)))
+
+(def missing-ids
+  (for [[n1 n2] (partition 2 1 ids)
+        :when (not (= (inc n1) n2))]
+    (inc n1)))
+
+(def part1 (apply max ids))
+(def part2 (first missing-ids))
+
+[part1 part2]
