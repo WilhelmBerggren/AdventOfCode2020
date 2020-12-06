cd2314e9cef5fade0e07ecda73ea5d5eba198e50
WilhelmBerggren <wilhelmberggren@hotmail.com>
2020-12-06 09:45:07 +0100

    unfinished c# day 4

---
+++ advent4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var data = File.ReadAllText(Environment.CurrentDirectory + "/../input4.txt");
+
+var items = data.Split("\n\n");
+Func<string, string[]> Itemize = item => new Regex(@"\s").Split(item);
+foreach (var item in items)
+{
+    var itemized = Itemize(item);
+    var parts = itemized.First().Split(":");
+    System.Console.WriteLine($"{parts[0]}: {parts[1]}");
+}
