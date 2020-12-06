17f87abf485462766722b67c10f4c2ec3bb99796
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-12-06 18:22:38 +0100

    simplified clj 6

---
+++ advent6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var groups = File
+    .ReadAllText(Environment.CurrentDirectory + "/../input6.txt")
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(group => group.Split(Environment.NewLine));
+
+var part1 = groups
+    .Select(group => group
+        .SelectMany(i => i)
+        .Distinct()
+        .Count())
+    .Sum();
+
+var part2 = groups
+    .Select(group => group
+        .SelectMany(i => i)
+        .Distinct()
+        .Where(q => group
+            .All(c => c
+                .Contains(q)))
+        .Count())
+    .Sum();
+
+System.Console.WriteLine(part1);
+System.Console.WriteLine(part2);
