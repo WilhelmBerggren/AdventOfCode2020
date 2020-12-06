ce34614514ae5b0ab862e382055a20c004834d8d
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-12-06 11:42:38 +0100

    initial clojure day 6

---
+++ advent6.clj
@@ -0,0 +1,11 @@
+(def data (str/split (slurp "input6.txt") #"\n\n"))
+
+(def groups (map #(set (str/split % #"\s")) data))
+
+(defn distinct-qs [group] (distinct (str/join "" group)))
+
+(def part1 (reduce + (map #(count (distinct-qs %)) groups)))
+      
+(def part2 (reduce + (map #(count (apply set/intersection (map set %))) groups)))
+
+[part1 part2]
