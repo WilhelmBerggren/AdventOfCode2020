fc77a29497b7070516d99cdb036d7a7f653b1a8c
WilhelmBerggren <wilhelmberggren@hotmail.com>
2021-01-01 20:34:15 +0100

    improved 19_5

--- 19_5.clj
+++ 19_5.clj
@@ -2,34 +2,30 @@
 
 (def instr-lengths {1 4, 2 4, 3 2, 4 2, 99 1})
 
-(defn next-instruction [nums]
-  (let [s (first nums)
-        s' (format "%05d" s)
-        instr (zipmap [:p3 :p2 :p1 :op] 
-                (map (fn [[a b]] (read-string (subs s' a b))) 
-                     '((0 1) (1 2) (2 3) (3 5))))
-        length (instr-lengths (:op instr))]
-    [instr (subvec nums 1 length)]))
+(defn parse-instr [num]
+  (let [s (format "%05d" num)]
+    (zipmap [:p3 :p2 :p1 :op] 
+            (map (fn [[a b]] (read-string (subs s a b))) 
+                 '((0 1) (1 2) (2 3) (3 5))))))
 
 (defn get-by-mode [mode arg nums]
   (if (= mode 1) arg (nth nums arg)))
 
-(defn exec-instr [instr args nums]
-  (case (:op instr)
-    1 (assoc nums (nth args 2)
-             (+ (get-by-mode (:p1 instr) (first args) nums)
-                (get-by-mode (:p2 instr) (second args) nums)))
-    2 (assoc nums (nth args 2)
-             (* (get-by-mode (:p1 instr) (first args) nums)
-                (get-by-mode (:p2 instr) (second args) nums)))
-    3 (assoc nums (first args) 1)
-    4 (do (println (get-by-mode (:p1 instr) (first args) nums)) nums)))
+(defn tick [[index nums]]
+  (let [instr (parse-instr (nth nums index))
+        index' (+ index (instr-lengths (:op instr)))
+        args (subvec nums (inc index) index')]
+    (case (:op instr)
+      1 [index' (assoc nums (nth args 2)
+                       (+ (get-by-mode (:p1 instr) (first args) nums)
+                          (get-by-mode (:p2 instr) (second args) nums)))]
+      2 [index' (assoc nums (nth args 2)
+                       (* (get-by-mode (:p1 instr) (first args) nums)
+                          (get-by-mode (:p2 instr) (second args) nums)))]
+      3 [index' (assoc nums (first args) 1)]
+      4 [index' (do (println (get-by-mode (:p1 instr) (first args) nums)) nums)]
+      99 [(count nums) nums])))
 
-(loop [index 0
-       nums nums]
-  (if (< (count nums) index) 
-    nums
-    (let [[instr args] (next-instruction (subvec nums index))
-          index' (+ index (instr-lengths (:op instr)))]
-      (if (= (:op instr) 99) nums
-          (recur index' (exec-instr instr args nums))))))
+(loop [state [0 nums]]
+  (when (< (first state) (count nums)) 
+    (recur (tick state))))
